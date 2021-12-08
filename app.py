62e867a748af79fe0850083c85174b92d0806244
arenag <arena.guiose@gmail.com>
2021-12-08 05:04:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -23,11 +23,23 @@ st.title("Airbnbs à Bordeaux")
 with st.container():
     st.write("This is inside the container")
 
-    # You can call any Streamlit command, including custom components:
-    st.bar_chart(np.random.randn(50, 3))
+    
 
     px.set_mapbox_access_token(mb_token)
 
-    fig = px.scatter_mapbox(df, lat="latitude", lon="longitude",  hover_data=["price", "availability_365"],   color="room_type", color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10)
+    fig = px.scatter_mapbox(df, lat="latitude", lon="longitude",  hover_data=["price", "availability_365"],   color="room_type",  size_max=15, zoom=10) #color_continuous_scale=px.colors.cyclical.IceFire,
 
     st.plotly_chart(fig)
+
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.header("A cat")
+    # You can call any Streamlit command, including custom components:
+    st.bar_chart(np.random.randn(50, 3))
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
