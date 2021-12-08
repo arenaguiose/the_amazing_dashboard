5a99e0f65504a5ba3a24fe7b2d341ad99f76bc8c
arenag <arena.guiose@gmail.com>
2021-12-08 04:52:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import plotly.express as px
 import geopandas as gpd
 
 
+
 df = pd.read_csv('data/listings.csv')
 nb = gpd.read_file('data/neighbourhoods.geojson')
 
@@ -13,8 +14,20 @@ df = df[df['room_type'] != "Hotel room"]
 
 
 mb_token = "pk.eyJ1IjoiYXJlbmFnc2UiLCJhIjoiY2t3d3hrMjl6MDg4dDMxcjBrbHBycHFqYSJ9.Izk3V1cMsjb96YruvGeMzg"
-px.set_mapbox_access_token(mb_token)
 
-fig = px.scatter_mapbox(df, lat="latitude", lon="longitude",  hover_data=["price", "availability_365"],   color="room_type", 
-                  color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10)
-fig.show()
+
+
+st.set_page_config(layout="wide")
+st.title("Airbnbs à Bordeaux")
+
+with st.container():
+    st.write("This is inside the container")
+
+    # You can call any Streamlit command, including custom components:
+    st.bar_chart(np.random.randn(50, 3))
+
+    px.set_mapbox_access_token(mb_token)
+
+    fig = px.scatter_mapbox(df, lat="latitude", lon="longitude",  hover_data=["price", "availability_365"],   color="room_type", color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10)
+
+    st.plotly_chart(fig)
